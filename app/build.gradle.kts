5a66ea72c30dbb83b73494e57e6aa257654f8e6e
saifsahil3 <saif.karish@gmail.com>
2026-09-02 14:43:33 -0400

    chore(release): bump version to 1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
     minSdk = 24
     targetSdk = 36
     versionCode = 1
-    versionName = "1.3.0"
+    versionName = "1.3.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
